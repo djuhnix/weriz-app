b167e19c514d0fe6e9199440330849e30da2ee01
germain <djuhngleen@gmail.com>
2022-04-21 18:14:18 +0200

    fix: add copyright

---
+++ src/components/footer/Copyright.tsx
@@ -0,0 +1,16 @@
+import {Typography} from "@mui/material";
+import {Link} from "react-router-dom";
+import {Routes} from "../../navigation/Routes";
+
+export const Copyright = (props: any) => {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 8, mb: 4 }} {...props}>
+      {'Copyright © '}
+      <Link color="inherit" to={Routes.root}>
+        {/* APP_NAME */} Accueil
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
