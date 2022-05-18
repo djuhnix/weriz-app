9cc0151255483bb691b31682f8a802fc50b50281
germain <djuhngleen@gmail.com>
2022-05-18 13:48:36 +0200

    feat: change index element of '/' route to loader

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { App } from 'src/app'
-import { AuthPage } from 'src/app/auth/AuthPage'
-import { Routes as Path } from './Routes'
-import { RequireAuth } from './RequireAuth'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { App } from 'src/app';
+import { AuthPage } from 'src/app/auth/AuthPage';
+import { Routes as Path } from './Routes';
+import { RequireAuth } from './RequireAuth';
+import { PageLoader } from '../components/loader/PageLoader';
+
 export interface AppNavigationProps {}
 
 export const AppNavigation: React.FC<AppNavigationProps> = () => {
   return (
     <Routes>
       <Route path={Path.root} element={<App />}>
-        <Route index element={<AuthPage />} />
+        <Route index element={<PageLoader />} />
         <Route path={Path.landing} element={<span>Landing</span>} />
         <Route path={Path.login} element={<AuthPage />} />
 
