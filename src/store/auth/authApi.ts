0fe659cd56175fcfc638c37952eb775a76d17c81
germain <djuhngleen@gmail.com>
2022-05-18 13:45:40 +0200

    feat: implement ApiResponse to rtk-query

--- src/store/auth/authApi.ts
+++ src/store/auth/authApi.ts
@@ -1,18 +1,19 @@
-import { baseQueryWithRetry } from 'src/store/query'
+import { ApiResponse, baseQuery } from 'src/store/query'
 import { UserModel } from '../user/userModel'
 import { AuthModel } from './authModel'
 import { createApi } from '@reduxjs/toolkit/query/react'
 
 export type Credentials = Pick<UserModel, 'email' | 'password'>
 
-export type AuthResponse = {
+export type AuthResponse = ApiResponse<{
   user: UserModel;
   tokenData: AuthModel['tokenData'];
-}
+}>
+
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: baseQueryWithRetry,
+  baseQuery: baseQuery,
   endpoints: (build) => ({
     login: build.mutation<AuthResponse, Credentials>({
       query: (credentials: Credentials) => ({ url: 'login', method: 'POST', body: credentials }),
@@ -23,4 +24,5 @@ export const authApi = createApi({
   }),
 });
 
+// export type LoginMutation = typeof authApi.endpoints.login.useMutation;
 export const { useLoginMutation, useSignupMutation } = authApi
