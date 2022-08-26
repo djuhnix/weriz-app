4a0e55b06c58aeb155b4ad9a6c95a000a0ac863e
djuhnix <djuhngleen@gmail.com>
2022-08-26 17:32:01 +0200

    feat: extract styles in auth

---
+++ src/app/auth/styles.ts
@@ -0,0 +1,12 @@
+export const boxStyles = {
+    root: {
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        m: 1,
+        bgColor: 'secondary.main',
+    },
+};
