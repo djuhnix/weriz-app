0bb23ec3d449704cd672ce7f828973725b1177da
djuhnix <djuhngleen@gmail.com>
2022-08-26 17:31:40 +0200

    feat: extract password input in individual component

--- src/app/auth/AuthPage.tsx
+++ src/app/auth/AuthPage.tsx
@@ -10,218 +10,169 @@ import { Routes } from 'src/navigation/Routes'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { Copyright } from 'src/components/footer/Copyright'
 import {
-  colors,
-  FormControl,
-  FormHelperText,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
+    colors,
 } from '@mui/material'
-import { Controller, useForm } from 'react-hook-form'
-import { Visibility, VisibilityOff } from '@mui/icons-material'
+import {Control, Controller, useForm} from 'react-hook-form'
 import * as z from 'zod'
-import { Config } from 'src/config'
 import { useLoginMutation, useSignupMutation } from 'src/store/auth/authApi'
 import { ApiStatusCode } from '../../enum/api'
 import { statusInRange } from '../../services/utils'
 import { selector } from '../../store'
 import { selectIsAuthenticated } from '../../store/auth/authSelector'
-// import { useAppSelector } from 'store'
-
-// TODO add verification
-const schema = z.object({
-  email: z.string({required_error: "L'adresse email est obligatoire"})
-    .email({ message: "Adresse email invalide" }),
-  password: z.string({required_error: "Le mot de passe est obligatoire"})
-    .regex(new RegExp(Config.PASSWORD_REGEX), "Votre mot de passe doit contenir au moins 8 caractères avec au moins 1 lettre majuscule, 1 lettre minuscule et 1 chiffre."),
-});
+import {boxStyles} from "./styles";
+import {validations} from "../../services/formsValidation";
+import {useBaseStyles} from "../baseStyles";
+import {isMobile} from "react-device-detect";
+import {PasswordInput} from "../../components/input/PasswordInput";
 
 interface Props {
-  mode?: 'signup' | 'login'
+    mode?: 'signup' | 'login'
 }
 
 export const AuthPage: FC<Props> = ({ mode = 'login'} : Props) => {
-  const defaultValues = {
-    email: '',
-    password: ''
-    // remember: false
-    // tos: false
-  }
-
-  const isAuthenticated = selector(selectIsAuthenticated);
-
-  const [login, loginResult] = useLoginMutation();
-  const [signup, signupResult] = useSignupMutation();
-
-  const [showPassword, setShowPassword] = useState(false);
-  const [submitError, setSubmitError] = useState('');
-
-  const result = mode == 'signup' ? signupResult : loginResult;
-
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const locState = location.state as { from: Location} | null;
-  const from = locState?.from?.pathname || Routes.rootFeed;
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate(from);
+    const defaultValues = {
+        username: '',
+        email: '',
+        password: ''
+        // remember: false
+        // tos: false
     }
-  }, []);
 
-  const { handleSubmit, control, formState: { errors } } = useForm({
-    defaultValues,
-    // resolver: zodResolver(schema)
-  });
+    const isAuthenticated = selector(selectIsAuthenticated);
 
-  const onSubmit = (data: any) => {
-    // const hashedPassword = bcrypt.hashSync(data.password) // hash created previously created upon sign up
-    // not necessary 'cause of https ?
-    if (mode == 'login') {
-      login({email: data.email, password: data.password})
-        .unwrap()
-        // after login navigate the user where he came from
-        .then(() => navigate(from))
-        .catch(error => {
-          if ('status' in error && statusInRange(error.status, ApiStatusCode.BAD_REQUEST)) {
-            setSubmitError("Vos identifiants sont incorrects");
-          }
-        })
-    } else {
-      signup({email: data.email, password: data.password})
-        .unwrap()
-        // after login navigate the user where he came from
-        .then(() => navigate(from))
-        .catch(error => {
-          if ('status' in error && statusInRange(error.status, ApiStatusCode.BAD_REQUEST)) {
-            setSubmitError("Une erreur est survenue, veuillez vérifier vos informations");
-          }
-        })
+    const [login, loginResult] = useLoginMutation();
+    const [signup, signupResult] = useSignupMutation();
+
+    const [submitError, setSubmitError] = useState('');
+
+    const { classes } = useBaseStyles();
+
+    const result = mode == 'signup' ? signupResult : loginResult;
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const locState = location.state as { from: Location} | null;
+    const from = locState?.from?.pathname || Routes.rootFeed;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate(from);
+        }
+    }, []);
+
+    const { handleSubmit, control, formState: { errors } } = useForm({
+        defaultValues,
+        // resolver: zodResolver(schema)
+    });
+
+    const onSubmit = (data: any) => {
+        // const hashedPassword = bcrypt.hashSync(data.password) // hash created previously created upon sign up
+        // not necessary 'cause of https ?
+        if (mode == 'login') {
+            login({username: data.username, password: data.password})
+                .unwrap()
+                // after login navigate the user where he came from
+                .then(() => navigate(from))
+                .catch(error => {
+                    if ('status' in error && statusInRange(error.status, ApiStatusCode.BAD_REQUEST)) {
+                        setSubmitError("Vos identifiants sont incorrects");
+                    }
+                })
+        } else {
+            signup({username: data.username, email: data.email, password: data.password})
+                .unwrap()
+                // after login navigate the user where he came from
+                .then(() => navigate(from))
+                .catch(error => {
+                    if ('status' in error && statusInRange(error.status, ApiStatusCode.BAD_REQUEST)) {
+                        setSubmitError("Une erreur est survenue, veuillez vérifier vos informations");
+                    }
+                })
+        }
     }
-  }
 
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
-
-  return (
-    <Box sx={boxStyles.root}>
-      <Avatar sx={boxStyles.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Weriz {mode == 'login' ? 'Connexion' : 'Inscription' }
-      </Typography>
-      <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate={true} sx={{ mt: 1 }}>
-        <Controller
-          name="email"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <TextField
-              error={Boolean(errors.email)}
-              helperText={errors?.email?.message}
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Adresse Email"
-              autoComplete="email"
-              autoFocus
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name="password"
-          control={control}
-          render={({field}) => (
-            <FormControl
-              error={Boolean(errors.password)}
-              fullWidth
-              required
-              margin="normal"
-              variant="outlined"
-            >
-              <InputLabel htmlFor="outlined-adornment-password">Mot de passe</InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                aria-describedby="password-helper-text"
-                type={showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
+    return (
+        <Box className={classes.roundContainer} sx={boxStyles.root}>
+            <Avatar sx={boxStyles.avatar}>
+                <LockOutlinedIcon/>
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Weriz {mode == 'login' ? 'Connexion' : 'Inscription'}
+            </Typography>
+            {/* TODO Remove novalidate */}
+            <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate={true} sx={{m: 2}}>
+                <Controller
+                    name="username"
+                    control={control}
+                    defaultValue=""
+                    render={({field}) => (
+                        <TextField
+                            error={Boolean(errors.username)}
+                            helperText={errors?.username?.message}
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Nom d'utilisateur"
+                            autoFocus
+                            {...field}
+                        />
+                    )}
+                />
+                {mode == 'signup' && (
+                    <Controller
+                        name="email"
+                        control={control}
+                        defaultValue=""
+                        render={({field}) => (
+                            <TextField
+                                error={Boolean(errors.email)}
+                                helperText={errors?.email?.message}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Adresse email"
+                                {...field}
+                            />
+                        )}
+                    />)}
+                <PasswordInput control={control} errors={errors} />
+                {result.isError && (<Typography align="center" color={colors.red.A400}> {submitError} </Typography>)}
+                <Box textAlign='center'>
+                    <LoadingButton
+                        loading={result.isLoading}
+                        type="submit"
+                        fullWidth={!result.isLoading}
+                        variant="contained"
+                        sx={{mt: 3, mb: 2}}
                     >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                {...field}
-                label="Password"
-              />
-              <FormHelperText id="password-helper-text">
-                {errors?.password?.message}
-              </FormHelperText>
-            </FormControl>
-          )}
-        />
-        {result.isError && (<Typography align="center" color={colors.red.A400}> {submitError} </Typography>)}
-        <Box textAlign='center'>
-          <LoadingButton
-            loading={result.isLoading}
-            type="submit"
-            fullWidth={!result.isLoading}
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            {mode == 'login' ? 'Se connecter' : "S'inscrire"}
-          </LoadingButton>
+                        {mode == 'login' ? 'Se connecter' : "S'inscrire"}
+                    </LoadingButton>
+                </Box>
+                <Grid container sx={{textAlign: 'center'}}>
+                    <Grid item xs={12} md={6} sx={{my: isMobile ? 1 : 0}}>
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            <Link to={Routes.rootForgotPassword} color="inherit">Mot de passe oublié ?</Link>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{my: isMobile ? 1 : 0}}>
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            {mode == 'login'
+                                ? <Link to={Routes.rootSignup} color="grey">{"Vous n'avez pas de compte ?"}</Link>
+                                : <Link to={Routes.rootLogin} color="grey">{"Vous avez déjà un compte ?"}</Link>}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Copyright/>
         </Box>
-        <Grid container>
-          <Grid item xs>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-            >
-              <Link to={Routes.rootForgetPassword} color="inherit">Mot de passe oublié ?</Link>
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-            >
-              {mode == 'login'
-                ? <Link to={Routes.rootSignup} color="grey">{"Vous n'avez pas de compte ?"}</Link>
-                : <Link to={Routes.rootLogin} color="grey">{"Vous avez déjà un compte ?"}</Link>}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
-      <Copyright />
-    </Box>
-  )
-}
-
-const boxStyles = {
-  root: {
-    marginTop: 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    m: 1,
-    bgColor: 'secondary.main',
-  },
+    )
 }
