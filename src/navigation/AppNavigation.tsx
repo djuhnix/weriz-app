d529d103db8d88d59bc245821891fcf3db619732
djuhnix <djuhngleen@gmail.com>
2022-08-26 17:33:40 +0200

    feat: add routes

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -4,6 +4,8 @@ import { App } from 'src/app';
 import { AuthPage } from 'src/app/auth/AuthPage';
 import { Routes as Path } from './Routes';
 import { RequireAuth } from './RequireAuth';
+import {ForgotPassword} from "../app/auth/ForgotPassword";
+import FeedPage from "../app/feed/FeedPage";
 
 export interface AppNavigationProps {
   hideLoader: () => void;
@@ -20,10 +22,12 @@ export const AppNavigation: React.FC<AppNavigationProps> = ({ hideLoader }) => {
         <Route path={Path.landing} element={<span>Landing</span>} />
         <Route path={Path.login} element={<AuthPage mode="login"/>} />
         <Route path={Path.signup} element={<AuthPage mode="signup"/>} />
+        <Route path={Path.forgotPassword} element={<ForgotPassword/>} />
+        <Route path={Path.resetPassword} element={<ForgotPassword mode="reset"/>} />
 
         <Route path={Path.feed} element={
           <RequireAuth>
-            <span>Feed</span>
+            <FeedPage />
           </RequireAuth>
         }/>
 
--- src/navigation/Routes.ts
+++ src/navigation/Routes.ts
@@ -2,12 +2,15 @@ export enum Routes {
   root = '/',
   rootLogin = '/login',
   rootSignup = '/signup',
-  rootForgetPassword = '/forget-password',
+  rootForgotPassword = '/forgot-password',
+  rootResetPassword = '/reset-password/:token',
   rootLanding = '/landing',
   rootFeed = '/feed',
-
   login = 'login',
+
   signup = 'signup',
   landing = 'landing',
+  forgotPassword = 'forgot-password',
+  resetPassword = 'reset-password/:token',
   feed = 'feed',
 }
